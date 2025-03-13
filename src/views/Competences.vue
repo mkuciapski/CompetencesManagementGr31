25dbf5efb874fbb579cefabd35b1bed938125f60
Michał Kuciapski <m.kuciapski@gmail.com>
2025-03-13 16:45:26 +0100

    ref init

--- src/views/Competences.vue
+++ src/views/Competences.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, onUpdated, ref } from 'vue'
+
+const headerTitle = ref('<i>Competences</i>')
+setTimeout(() => {
+  headerTitle.value = 'I love reactivity'
+}, 4000)
+
+//#region EVENTS
+onUpdated(() => {
+  console.log('Updated - rerendered )')
+})
+
+onMounted(() => {
+  console.log('Mounted - component mounted (Jquery)')
+})
+
+console.log('Created - after created (AJAX)')
+//#endregion
+</script>
 
 <template>
-  <h1>Competences</h1>
+  <h1><span v-html="headerTitle"></span></h1>
 </template>
 
 <style scoped></style>
