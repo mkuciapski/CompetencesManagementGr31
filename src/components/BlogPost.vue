62d9c6ccfbca6ba8e265708ee2db86dd1fa29081
Michał Kuciapski <m.kuciapski@gmail.com>
2025-04-03 12:53:49 +0200

    sfc; props initial

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
-defineProps(['text', 'author', 'type'])
+// defineProps(['text', 'author', 'type'])
+// defineProps({
+//   text: {
+//     type: String,
+//     required: true,
+//     validator: (v: string) => v.length > 2,
+//   },
+//   author: {
+//     type: String,
+//     required: true,
+//     default: 'unknown',
+//   },
+//   type: {},
+// })
+withDefaults(defineProps<{ text: string; author?: string; type?: string }>(), {
+  author: 'unknown',
+})
 </script>
 
 <template>
